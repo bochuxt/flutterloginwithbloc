f86a57b8ad1f54ef8020bcdddd860b6274925ea8
george <bochuxt7@gmail.com>
2019-06-05 18:59:57 -0700

    fix

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:app/myform.dart';
+
+main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Form Validation')),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: MyForm(),
+        ),
+      ),
+    );
+  }
+}
+
